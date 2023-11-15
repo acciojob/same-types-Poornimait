17bd6e48c9a8a988714ce87a45d33056facd1798
poornima reddy <54319456+Poornimait@users.noreply.github.com>
2023-11-15 12:17:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function isSameType(value1, value2) {
   //your js code here
+	if(typeof(value1)==typeof(value2)){
+		console.log("true");
+	}
+	else{
+		console.log("true");
+	}
+	
 }
 
 // do not change the code below.
